f085181e89cb86ec41f6c2c2d36aa7be4eb72e51
rmoooon <rmontanezp@unsa.edu.pe>
2026-09-05 22:21:32 -0500

    add cubic solver implementation and random data generation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use rand::{RngExt, SeedableRng, rngs::StdRng};
+
 trait MaximumSubArraySolver {
-    fn max_sum(&self) -> i32;
+    fn max_sum(&self, arr: &[i32]) -> i32;
 }
 
 struct CubicSolver;
 
 impl MaximumSubArraySolver for CubicSolver {
-    fn max_sum(&self) -> i32 {
-        todo!()
+    fn max_sum(&self, arr: &[i32]) -> i32 {
+        let mut best = i32::MIN;
+        let size = arr.len();
+        for i in 1..size {
+            for j in i..size {
+                let mut sum = 0;
+
+                for k in &arr[i..j] {
+                    sum += k;
+                }
+
+                if sum > best {
+                    best = sum;
+                }
+            }
+        }
+
+        best
     }
 }
+
 struct QuadraticSolver;
 
 impl MaximumSubArraySolver for QuadraticSolver {
-    fn max_sum(&self) -> i32 {
+    fn max_sum(&self, arr: &[i32]) -> i32 {
         todo!()
     }
 }
@@ -20,10 +41,17 @@ impl MaximumSubArraySolver for QuadraticSolver {
 struct KadaneSolver;
 
 impl MaximumSubArraySolver for KadaneSolver {
-    fn max_sum(&self) -> i32 {
+    fn max_sum(&self, arr: &[i32]) -> i32 {
         todo!()
     }
 }
+
+fn gen_data(size: usize) -> Vec<i32> {
+    let mut rng = StdRng::seed_from_u64(7);
+    (0..size).map(|_| rng.random_range(-100..100)).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = gen_data(20);
+    data.iter().for_each(|x| print!("{x} | "));
 }
