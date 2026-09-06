8976c314b90ced06615e9f6670918d31ae4d91ff
rmoooon <rmontanezp@unsa.edu.pe>
2026-09-05 21:47:22 -0500

    initialize project setup

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+trait MaximumSubArraySolver {
+    fn max_sum(&self) -> i32;
+}
+
+struct CubicSolver;
+
+impl MaximumSubArraySolver for CubicSolver {
+    fn max_sum(&self) -> i32 {
+        todo!()
+    }
+}
+struct QuadraticSolver;
+
+impl MaximumSubArraySolver for QuadraticSolver {
+    fn max_sum(&self) -> i32 {
+        todo!()
+    }
+}
+
+struct KadaneSolver;
+
+impl MaximumSubArraySolver for KadaneSolver {
+    fn max_sum(&self) -> i32 {
+        todo!()
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}
